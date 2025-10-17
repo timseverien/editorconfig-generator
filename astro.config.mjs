c3ddde87ef7cf762dec6bd4a729b525e421ab71c
Tim Severien <timseverien@gmail.com>
2025-10-17 10:41:31 +0200

    chore: clean up configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import svelte from '@astrojs/svelte';
 import playformCompress from '@playform/compress';
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
 	integrations: [svelte(), playformCompress()],
 });
